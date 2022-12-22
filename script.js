961b9d4af2dacd0aed5d9f99c18e04cf5f1c73d0
Thirumalaikumar M <111740460+Thirumalaikumar-M@users.noreply.github.com>
2022-12-22 22:16:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+    $(window).on('load scroll',function(){
+
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        $('.login-form').removeClass('popup');
+
+        $('section').each(function(){
+
+            let top = $(window).scrollTop();
+            let height = $(this).height();
+            let id = $(this).attr('id');
+            let offset = $(this).offset().top - 200;
+
+            if(top > offset && top < offset + height){
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+
+
+        });
+
+    });
+
+}); 
+
+
